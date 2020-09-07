56b017d019c8e45482b3cdc43e344abccabfcece
Viacheslav Kaskov <Dr.Sfar@yandex.ru>
2020-09-07 20:14:41 +0300

    Первоначальная настройка webpack

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    overlay: true,
+    port: 8081
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          }
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+    new CleanWebpackPlugin({cleanStaleWebpackAssets: false ,}),
+    new HtmlWebpackPlugin ({
+      hash: false,
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new HtmlWebpackPlugin ({
+      hash: false,
+      filename: 'assets/assets.html',
+      template: './src/assets/assets.html',
+    }),
+  ],
+}
