9488be0f6886792797b1b1168ef5f41f27fc44c8
Viacheslav Kaskov <Dr.Sfar@yandex.ru>
2020-09-28 17:35:48 +0300

    Написан базовый class с простешими вычислениями положения(с ошибками)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,56 @@
 import './index.scss';
 
-function sum(a: number, b: number) {
-  console.log(a);
-  console.log(b);
-  console.log(a + b);
-  return a + b;
+class Slider {
+  classElem: string;
+
+  elem: HTMLElement;
+
+  shiftX: number;
+
+  shiftY: number;
+
+  constructor(name: string) {
+    this.classElem = name;
+    this.elem;
+    this.shiftX;
+    this.shiftY;
+  }
+
+  sliderInit() {
+    this.elem = document.querySelector(this.classElem);
+    this.elem.addEventListener('mousedown', this.dragAndDrop.bind(this));
+    this.elem.ondragstart = () => false;
+  }
+
+  dragAndDrop(event: MouseEvent) {
+    this.shiftX = event.clientX - this.elem.getBoundingClientRect().left;
+    this.shiftY = event.clientY;
+
+    this.elem.style.position = 'absolute';
+    this.elem.style.zIndex = '1000';
+    // document.body.append(this.elem);
+
+    document.addEventListener('mousemove', this.onMouseMove.bind(this));
+    console.log('======');
+    document.addEventListener('mouseup', this.removeListener.bind(this));
+  }
+
+  onMouseMove(event: MouseEvent) {
+    this.moveAt(event.pageX);
+  }
+
+  moveAt(pageX: number) {
+    this.elem.style.left = `${pageX - this.shiftX}px`;
+    this.elem.style.top = `${this.shiftY}px`;
+  }
+
+  removeListener() {
+    document.removeEventListener('mousemove', this.onMouseMove);
+    this.elem.addEventListener('mouseup', null);
+    console.log('событие отработало');
+  }
 }
 
-sum(1, 2);
+const mySlidr = new Slider('.slider__thumb');
 
-const people: string = 'ПРИВЕТ РЕБЯТА ВСЕМ НА СВЯЗИ ЁПСТУДЕЙ КАК МАЛО НА ЗЕМЛЕ ПОРЯДОЧЫХ ЛЮДЕЙ';
-console.log(people);
-module.exports = sum;
+mySlidr.sliderInit();
