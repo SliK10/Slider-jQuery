b5955ac184e0b05402303320a78556ac9cbc7653
Viacheslav Kaskov <Dr.Sfar@yandex.ru>
2020-10-25 19:32:05 +0300

    Добавил автоматическую доводку thumb до края полосы, при резком движении курсора за ее пределы

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,18 @@ thumb.onmousedown = function (event: MouseEvent) {
   const shiftX = event.clientX - thumb.getBoundingClientRect().left;
   function moveAt(event: MouseEvent) {
     const sliderBorderLeft = slider.getBoundingClientRect().left;
-    const sliderWidth = slider.getBoundingClientRect().right - sliderBorderLeft;
-    const thumbWidth = thumb.getBoundingClientRect().right - thumb.getBoundingClientRect().left;
-    const eventBorderLeft = event.clientX - sliderBorderLeft - shiftX;
+    const eventBorderLeft = event.pageX - sliderBorderLeft - shiftX;
 
-
-    console.log(sliderWidth - thumbWidth)
+    console.log(eventBorderLeft);
+    console.log(event);
     if (eventBorderLeft >= 0 && eventBorderLeft <= 286) {
-      thumb.style.left = `${(event.clientX - sliderBorderLeft - shiftX)}px`;
+      thumb.style.left = `${(eventBorderLeft)}px`;
+    } else if (eventBorderLeft > 286) {
+      thumb.style.left = `${286}px`;
+    } else if (eventBorderLeft < 0) {
+      thumb.style.left = `${0}px`;
     }
   }
-
-  // moveAt(event);
 
   document.addEventListener('mousemove', moveAt);
 
